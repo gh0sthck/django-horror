fd569438fb4fb4e2027f2ea2e58df9f0ae10725d
~gh0sthck <96289997+gh0sthck@users.noreply.github.com>
2025-09-08 09:39:31 +0000

    Add custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=90, verbose_name="Имя пользователя", unique=True, null=False)
+    bio = models.TextField(verbose_name="О себе", max_length=256, blank=True, default="")
+    email = models.EmailField(verbose_name="Электронная почта", unique=True)
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата регистрации")
+
+    def __repr__(self) -> str:
+        return f"<CustomUser: {self.username}>"
+
+    def __str__(self) -> str:
+        return self.username
+
+    class Meta:
+        ordering = ["-username"]
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
