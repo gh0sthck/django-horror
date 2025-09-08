0509d6851334cc6fc264deb2cb99db64670a7e72
~gh0sthck <96289997+gh0sthck@users.noreply.github.com>
2025-09-08 10:51:24 +0000

    Add comments models

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comments
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ["comment", "user"]
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["title", "author"]
+    list_display = ["title", "author", "created_date"]
--- posts/models.py
+++ posts/models.py
@@ -3,13 +3,30 @@ from django.db import models
 from users.models import CustomUser
 
 
+class Comments(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="Автор комментария")
+    comment = models.TextField(verbose_name="Текст комментария", null=False)
+    # answer = models.ManyToManyField("Comments", null=True, blank=True) (?) to comment threads
+
+    def __repr__(self) -> str:
+        return f"<Comment: {self.user.username}: {self.comment[:20]}>"
+    
+    def __str__(self) -> str:
+        return self.comment[:20]
+
+    class Meta:
+        ordering = ["pk"]
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комменатрии"
+
+
 class Post(models.Model):
     title = models.CharField(max_length=90, verbose_name="Имя поста", unique=True, null=False)
     text = models.TextField(verbose_name="Содержание", null=False)  # temporary - field will updated
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="Автор")
     # TODO: views - redis array
     # TODO: likes - redis array
-    # TODO: comments field
+    comments = models.ManyToManyField(Comments, verbose_name="Комментарии", blank=True)
     created_date = models.DateField(auto_now_add=True, verbose_name="Дата создания")
     updated_date = models.DateField(auto_now_add=True, verbose_name="Дата изменения")
 
@@ -19,7 +36,16 @@ class Post(models.Model):
     def __repr__(self) -> str:
         return f"<Post: {self.title}>"
     
+    def get_comments(self) -> list[Comments]:
+        return self.comments.all()
+    
+    def get_comments_count(self) -> int:
+        return len(self.comments.all())
+
     class Meta:
         ordering = ["-title"]
         verbose_name = "Пост"
         verbose_name_plural = "Посты"
+
+
+
