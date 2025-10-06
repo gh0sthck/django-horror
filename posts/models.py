6876789103278f935abcca6938e43ce379256770
gh0st <maratka2202@gmail.com>
2025-10-06 12:47:16 +0300

    Styles update

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ from users.models import CustomUser
 
 class Comments(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="Автор комментария")
-    comment = models.TextField(verbose_name="Текст комментария", null=False)
+    comment = models.TextField(verbose_name="Оставьте свой комментарий", null=False)
     answer = models.ManyToManyField("Comments", blank=True) # (?) to comment threads
     # TODO: likes to commnt (redis?)
     date = models.DateTimeField(verbose_name="Дата комменатрия", auto_now_add=True)
